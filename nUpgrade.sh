fdea1aa9b7e2aada545bf1ee9039092d7feba7fc
Lokarin <49108855+Lokarin@users.noreply.github.com>
2020-06-06 14:14:48 -0300

    Delete nUpgrade.sh

--- nUpgrade.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-case $BLOCK_BUTTON in
-	1) notify-send "$(apt list --upgradable 2>/dev/null | sed '1d' | awk '{print $1 }')" ;;
-	#2) echo "Botão esquerdo" ;;
-esac
-
-apt list --upgradable 2>/dev/null | sed '1d' | wc - | awk '{ print $1 }'
