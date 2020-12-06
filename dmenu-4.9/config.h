dff9ae1484b48c5ddf2a48280fc1909baef2fafc
Lokarin <onukiamaral2@gmail.com>
2020-12-06 20:09:03 -0300

    Update na build do dmenu

--- dmenu-4.9/config.h
+++ dmenu-4.9/config.h
@@ -6,7 +6,7 @@ static int centered = 0;
 static int min_width = 500;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"System San Francisco Display:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
